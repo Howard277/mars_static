963d5a2ca6d81790e4897bcccd4ac746d4e6a12b
wuketao <wuketao@houbank.com>
2019-04-03 18:08:33 +0800

    优化菜单

--- src/App.vue
+++ src/App.vue
@@ -9,12 +9,8 @@
       text-color="#fff"
       active-text-color="#ffd04b"
     >
-      <el-menu-item index="1">
-        <router-link to="/">主页</router-link>
-      </el-menu-item>
-      <el-menu-item index="2">
-        <router-link to="/customer">客户信息</router-link>
-      </el-menu-item>
+      <!--菜单的组成是通过data中的menus数组构成的-->
+      <el-menu-item v-for="item in menus" :key="item.index" :index="item.index">{{item.name}}</el-menu-item>
     </el-menu>
     <router-view></router-view>
   </div>
@@ -23,20 +19,29 @@
 <script>
 export default {
   name: 'App',
-  data () {
+  data() {
     return {
       menus: [{
-        'name': '处理中心'
+        'index': 'mainpage',
+        'name': '主页',
+        'path': '/'
       }, {
-        'name': '用户中心'
+        'index': 'usercenter',
+        'name': '用户中心',
+        'path': '/customer'
       }],
-      activeIndex: '1',
-      activeIndex2: '1'
+      activeIndex: 'mainpage',
+      activeIndex2: 'mainpage'
     }
   },
   methods: {
-    handleSelect: function () {
-
+    // 菜单变更处理函数，遍历data中的menus数据，判断当前选中菜单的index数据然后调整的指定路径
+    handleSelect: function (key, keyPath) {
+      for(let idx in this.$data['menus']){
+        if(this.$data['menus'][idx]['index'] === key){
+          this.$router.push(this.$data['menus'][idx]['path'])
+        }
+      }
     }
   }
 }
